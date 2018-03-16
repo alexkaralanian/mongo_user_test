d2862c682cfa6dc94d8371c312fff0a0c2e5bf99
InRhythm_052 <inrhythm_052@Admins-MacBook-Pro-2.local>
2018-03-16 15:59:03 -0400

    init commit

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,20 +2,33 @@ const assert = require('assert')
 const User = require('../src/user')
 
 describe('Reading users out of db', () => {
-	let joe;
+	// let joey;
+	// how are these tests still passing?
+	// beforeEach gives each 'it' statement access to Joe
 
-	beforeEach( done => {
+	beforeEach(done => {
 		joe = new User ({name: 'Joe'})
 		joe.save()
 			.then(() => done())
 	})
 
-	it('finds all users with a name of joe', () => {
+	it('finds all users with a name of joe', done => {
 		User.find({
 			name: 'Joe'
 		})
 		.then(returnedUser => {
-			console.log('USER', returnedUser)
+			console.log('ID', joe.id)
+			assert(returnedUser[0].id === joe.id)
+			done()
+		})
+	})
+
+	it('finds user with a specific ID', done => {
+		User.findOne({
+			_id: joe.id
+		})
+		.then(returnedUser => {
+			assert(returnedUser.id === joe.id)
 			done()
 		})
 	})
